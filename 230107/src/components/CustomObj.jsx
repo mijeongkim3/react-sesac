3df476dd0d3c52b8b302417e040dbe30d63996fa
mijeongkim3 <yellowredcandy3@gmail.com>
2023-01-07 18:02:25 +0900

    2023010777

--- 230107/src/components/CustomObj.jsx
+++ 230107/src/components/CustomObj.jsx
@@ -32,4 +32,4 @@ export default function CustomObj(props) {
 //callcustomobj랑연결//객체 프롭스 전달
 
 //if랑 for문은 리턴안에서는 사용불가능함 jsx라서....그래서 삼항연산자씀
-//아예 큰상황이바뀔대는 if문쓰고 return을 그안에 넣는식으로 해야댐 (지금처럼)..
+//아예 큰상황이바뀔대는 if문쓰고 return을 그안에 넣는식으로 해야댐 (지금처럼)...
