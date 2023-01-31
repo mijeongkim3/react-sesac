e9fcbe81be1a94f5ca28f79c801e8c92ea36968d
mijeongkim3 <yellowredcandy3@gmail.com>
2023-01-31 18:50:00 +0900

    feat:2023013111111

--- 230110/src/components/Ex8.jsx
+++ 230110/src/components/Ex8.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import ChildrenEx8 from './ChildrenEx8'
+import React from 'react';
+import ChildrenEx8 from './ChildrenEx8';
 import book from '../book.jpg';
 
 export default function Ex8() {
-    const bookObj ={
-        title: '나의 하루는 4시 40분에 시작된다',
-        author: '김유진',
-        price: '13500원',
-        type: '자기계발서'
-    }
-     const style = {
-        backgroundColor: "yellow",
-        
-     }
+  const bookObj = {
+    title: '나의 하루는 4시 40분에 시작된다',
+    author: '김유진',
+    price: '13500원',
+    type: '자기계발서',
+  };
+  const style = {
+    backgroundColor: 'yellow',
+  };
   return (
-    <div style= {style}>
-        <div className = "head" >이번주 베스트셀러</div>
-        <img src={book} alt="이미지" />
-        <ChildrenEx8 obj={bookObj}/>
+    <div style={style}>
+      <div className="head">이번주 베스트셀러</div>
+      <img src={book} alt="이미지" />
+      <ChildrenEx8 obj={bookObj} />
     </div>
-  )
+  );
 }
 
-//app.js에서 ex8불러오면됨 이게부모...,.
+//app.js에서 ex8불러오면됨 이게부모...,..
